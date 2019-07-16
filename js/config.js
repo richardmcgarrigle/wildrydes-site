15852194578828887449535d3caf942986b261f7
Richard <richard.mcgarrigle@gmail.com>
2019-07-16 11:11:30 +0000

    config setting for userPoolID

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_AIEhLauVp', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4g63657q4vu58hrep5kbsgbp0k', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
